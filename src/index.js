eaee28736f6a2df8c1b50f31d7f8e0718ce0a975
William Jiang <williamjxj@hotmail.com>
2018-09-25 12:23:55 +0800

    step3: add redux.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './step2/App';
+import App from './step3/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+// Reducer
+function counter(state = { count: 0 }, action) {
+    const count = state.count
+    switch (action.type) {
+        case 'increase':
+            return { count: count + 1 }
+        default:
+            return state
+    }
+}
+
+// Store
+const store = createStore(counter)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+)
+
 registerServiceWorker();
